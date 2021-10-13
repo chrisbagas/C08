5a9cb9cc17621caea99233277eb9ab88d5bbce10
Christopher Bagas Laiputra <christopher.bagas@ui.ac.id>
2021-10-13 20:34:30 +0700

    set

--- C08/C08/settings.py
+++ C08/C08/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-c$2p0q7@7a5$my97wd&vb-5r0p5158wgf5if@5&qht%rd4@p8)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','C08.herokuapp.com']
 
 
 # Application definition
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'C08.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
