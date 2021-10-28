798731a6bb72dc62c60768c01d2801ae88ac2f61
Visianita Widyaningrum <visianitawidya@gmail.com>
2021-10-28 23:27:24 +0700

    update profile dashboard

--- C08/settings.py
+++ C08/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'home',
     'event',
     'leaderboard',
+    'profile_dashboard'
 ]
 
 MIDDLEWARE = [
--- C08/urls.py
+++ C08/urls.py
@@ -21,11 +21,12 @@ from django.conf import settings
 import home.urls as home
 import event.urls as event
 import leaderboard.urls as leaderboard
+import profile_dashboard.urls as profile
 from home.views import index as index_home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(home)),
-    path('event/', include(event)),
+    path('profile/', include(profile)),
     re_path(r'^$', index_home, name='index')
 ]
