38d8ec589c65515fba16e46bf9acac7d8f951f82
Visianita Widyaningrum <visianitawidya@gmail.com>
2021-10-29 14:45:08 +0700

    fix profile model

--- profile_dashboard/models.py
+++ profile_dashboard/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from PIL import Image
-
 
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
