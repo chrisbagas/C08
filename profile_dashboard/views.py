c7b9d120e1dcfacc9556328a613f7b1ad1746a0b
Visianita Widyaningrum <visianitawidya@gmail.com>
2021-10-29 14:21:47 +0700

    update form edit profile

--- profile_dashboard/views.py
+++ profile_dashboard/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Profile
 from .forms import UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
 
 @login_required
 def profile(request):
-    #update profile
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            return redirect('profile')
 
-    context = {}
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
 
-    return render(request, "edit_profile.html", context)
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, 'edit_profile.html', context)
