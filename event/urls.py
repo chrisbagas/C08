00128982d80d6c1b457f6187fb469db0240da305
Christopher Bagas Laiputra <christopher.bagas@ui.ac.id>
2021-10-29 10:38:26 +0700

    update event

--- event/urls.py
+++ event/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import index,event_detail,event_form
 
+app_name="event"
 urlpatterns = [
     #Menambahkan path sesuai dengan urls
     path('', index, name='index'),
