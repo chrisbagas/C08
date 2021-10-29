fce400ab412c7807cea12360354ed761ffabaf30
Visianita Widyaningrum <visianitawidya@gmail.com>
2021-10-29 17:15:56 +0700

    fix views profile

--- profile_dashboard/views.py
+++ profile_dashboard/views.py
@@ -1,30 +1,27 @@
 from django.shortcuts import redirect, render
 from .models import Profile
 from .forms import UserUpdateForm, ProfileUpdateForm
-#from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def profile(request):
-    if request.user.is_authenticated:
-        u_form = UserUpdateForm()
-        p_form = ProfileUpdateForm()
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                    request.FILES,
+                                    instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            return redirect('profile')
 
-        if request.method == 'POST':
-            u_form = UserUpdateForm(request.POST, instance=request.user)
-            p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
-            if u_form.is_valid() and p_form.is_valid():
-                u_form.save()
-                p_form.save()
-                return redirect('profile')
-
-        else:
-            u_form = UserUpdateForm(instance=request.user)
-            p_form = ProfileUpdateForm(instance=request.user.profile)
-
-        context = {
-            'u_form': u_form,
-            'p_form': p_form
-        }
-
-        return render(request, 'edit_profile.html', context)
     else:
-        return redirect('signup')
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, 'users/profile.html', context)
