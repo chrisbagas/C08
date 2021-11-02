1228fd056ddf04340e3377547124321a011d9cfa
Muhammad Hafidz S <muhammad.hafidz05@ui.ac.id>
2021-11-02 09:55:02 +0700

    Update admin, models, forms, urls and views for mini survey

--- mini_survey/forms.py
+++ mini_survey/forms.py
@@ -1,14 +1,33 @@
 from django import forms
-from .models import Survey
+from .models import Option, Survey
 
 
 class CreateSurveyForm(forms.ModelForm):
+    option1 = forms.CharField(label='Option 1', max_length=75)
+    option2 = forms.CharField(label='Option 2', max_length=75)
+
     class Meta:
         model = Survey
         fields = [
             'title',
             'description',
-            'first_option',
-            'second_option',
-            'third_option',
+            'option1',
+            'option2'
+        ]
+
+
+class EditSurveyForm(forms.ModelForm):
+    class Meta:
+        model = Survey
+        fields = [
+            'title',
+            'description',
+        ]
+
+
+class AddOptionForm(forms.ModelForm):
+    class Meta:
+        model = Option
+        fields = [
+            'text',
         ]
--- mini_survey/models.py
+++ mini_survey/models.py
@@ -1,15 +1,44 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
 
 class Survey(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=300)
-    first_option = models.CharField(max_length=50)
-    second_option = models.CharField(max_length=50)
-    third_option = models.CharField(max_length=50)
-    first_option_count = models.IntegerField(default=0)
-    second_option_count = models.IntegerField(default=0)
-    third_option_count = models.IntegerField(default=0)
+    creator = models.ForeignKey(
+        User, on_delete=models.CASCADE, blank=True, null=True)
+    pub_date = models.DateTimeField(default=timezone.now)
+
+    def vote_requirement(self, user):
+        # Check if user already voted
+        user_votes = user.vote_set.all()
+        if user_votes.filter(survey=self).exists():
+            return False
+        return True
+
+    def get_survey_count(self):
+        return self.vote_set.count()
 
     def __str__(self):
         return self.title
+
+
+class Option(models.Model):
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    text = models.CharField(max_length=75)
+
+    def get_option_count(self):
+        return self.vote_set.count()
+
+    def __str__(self):
+        return self.text
+
+
+class Vote(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    option = models.ForeignKey(Option, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
