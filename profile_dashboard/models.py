2aa1edefba1af26fc17796a274ee5882b3f1bd77
Visianita Widyaningrum <visianitawidya@gmail.com>
2021-11-03 18:21:00 +0700

    update profile

--- profile_dashboard/models.py
+++ profile_dashboard/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
-    bio = models.TextField()
+    bio = models.TextField(null=True, blank=True)
     image = models.ImageField(null=True,blank=True,upload_to='images/profile_dashboard')
 
     def __str__(self):
