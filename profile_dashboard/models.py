e429396cbb19d92adf9553dadffef5990c235b67
Visianita Widyaningrum <visianitawidya@gmail.com>
2021-11-03 18:47:13 +0700

    update profile model

--- profile_dashboard/models.py
+++ profile_dashboard/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
-    bio = models.TextField(null=True, blank=True)
-    image = models.ImageField(null=True,blank=True,default='default_profpic.png',upload_to='images/profile_dashboard')
+    bio = models.CharField(null=True, blank=True, default="", max_length= 200)
+    image = models.ImageField(null=True, blank=True, default='default_profpic.png',upload_to='images/profile_dashboard')
 
     def __str__(self):
         return f'{self.user.username} Profile'
