2d023e5d751a486cc6020ad5c96eae34cb009bd0
Muhammad Hafidz S <muhammad.hafidz05@ui.ac.id>
2021-11-04 14:18:19 +0700

    Add and update some templates, also fix views in mini survey

--- mini_survey/forms.py
+++ mini_survey/forms.py
@@ -3,8 +3,10 @@ from .models import Option, Survey
 
 
 class CreateSurveyForm(forms.ModelForm):
-    option1 = forms.CharField(label='Option 1', max_length=75)
-    option2 = forms.CharField(label='Option 2', max_length=75)
+    option1 = forms.CharField(label='Option 1', max_length=75,
+                              widget=forms.TextInput(attrs={'class': 'form-control'}))
+    option2 = forms.CharField(label='Option 2', max_length=75,
+                              widget=forms.TextInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = Survey
@@ -14,6 +16,10 @@ class CreateSurveyForm(forms.ModelForm):
             'option1',
             'option2'
         ]
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3, 'cols': 20}),
+        }
 
 
 class EditSurveyForm(forms.ModelForm):
