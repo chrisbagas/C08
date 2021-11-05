2673345044dcc552e8229fbe827e40a50a4e50d9
Andhira H. Sikoko <70933622+andhi30@users.noreply.github.com>
2021-11-05 22:27:48 +0700

    Add files via upload

--- forum/urls.py
+++ forum/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from forum.views import detailView, homeView
+from forum.views import createComment, createForum, deleteForum, detailView, homeView
 
 urlpatterns = [
     path("", homeView, name="home"),
-    path("forum/<int:pk>", detailView, name="forum_detail"),
+    path('create',  createForum.as_view(), name='forum_create'),
+    path('comment/create',  createComment.as_view(), name='comment_create'),
+    path('delete',  deleteForum.as_view(), name='forum_delete'),
+    path("<int:pk>", detailView, name="forum_detail"),
 ]
