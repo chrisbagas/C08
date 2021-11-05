d949b66ca0110a7e0877f22f20ba6591ac46b79d
Vincent Yovian <70625944+Spacesick@users.noreply.github.com>
2021-11-05 21:23:25 +0700

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -32,7 +32,7 @@ def add_post(request):
 @login_required(login_url='/login/')
 def edit_post(request, slug):
     post = BlogPost.objects.get(slug=slug)
-    form = BlogPostForm(request.POST, request.FILES, instance=post)
+    form = BlogPostForm({request.POST, request.FILES}, instance=post)
     if request.method == 'POST' and form.is_valid:
         temp = form.save(commit=False)
         temp.save()
