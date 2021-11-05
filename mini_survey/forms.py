a71ac21b307be360680fbb5f5c7d306e39ac1e4e
Muhammad Hafidz S <muhammad.hafidz05@ui.ac.id>
2021-11-05 12:38:31 +0700

    Update forms, views and styling for templates

--- mini_survey/forms.py
+++ mini_survey/forms.py
@@ -4,9 +4,9 @@ from .models import Option, Survey
 
 class CreateSurveyForm(forms.ModelForm):
     option1 = forms.CharField(label='Option 1', max_length=75,
-                              widget=forms.TextInput(attrs={'class': 'form-control'}))
+                              widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Option 1'}))
     option2 = forms.CharField(label='Option 2', max_length=75,
-                              widget=forms.TextInput(attrs={'class': 'form-control'}))
+                              widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Option 2'}))
 
     class Meta:
         model = Survey
@@ -17,8 +17,8 @@ class CreateSurveyForm(forms.ModelForm):
             'option2'
         ]
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3, 'cols': 20}),
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 4, 'cols': 20, 'placeholder': 'Description'}),
         }
 
 
@@ -29,6 +29,10 @@ class EditSurveyForm(forms.ModelForm):
             'title',
             'description',
         ]
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 4, 'cols': 20, 'placeholder': 'Description'}),
+        }
 
 
 class AddOptionForm(forms.ModelForm):
@@ -37,3 +41,6 @@ class AddOptionForm(forms.ModelForm):
         fields = [
             'text',
         ]
+        widgets = {
+            'text': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Option text'})
+        }
