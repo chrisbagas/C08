096f2519992c13848830e0b8b9bd19025db335b3
Vincent Yovian <70625944+Spacesick@users.noreply.github.com>
2021-11-05 23:39:48 +0700

    Update forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -8,6 +8,6 @@ class BlogPostForm(forms.ModelForm):
         widgets = {
             'title': forms.TextInput(attrs={'class': 'focus:ring-indigo-500 focus:border-indigo-500 flex-1 block rounded-md w-full border-gray-300', 'placeholder': 'Write the title for your blog post here.'}),
             'subtitle': forms.TextInput(attrs={'class': 'focus:ring-indigo-500 focus:border-indigo-500 flex-1 block rounded-md w-full border-gray-300', 'placeholder': 'Write an appealing subtitle for your blog post here.'}),
-            'thumbnail': forms.FileInput(attrs={'class': 'mt-1 w-full flex justify-center rounded-md py-2 px-3 shadow-sm text-sm font-medium text-white bg-indigo-600'}),
+            'thumbnail': forms.FileInput(attrs={'class': 'mt-1 w-full flex justify-center rounded-md py-2 px-3 shadow-sm text-sm font-medium text-white bg-green-600'}),
             'body': forms.Textarea(attrs={'class': 'shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full border border-gray-300 rounded-md', 'placeholder': 'Write your blog contents here.', 'rows': 16})
         }
