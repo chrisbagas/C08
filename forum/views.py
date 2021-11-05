72ebbf99782c804e5601e87a56964511d1f45083
Andhira H. Sikoko <70933622+andhi30@users.noreply.github.com>
2021-11-05 22:28:38 +0700

    Add files via upload

--- forum/views.py
+++ forum/views.py
@@ -1,7 +1,13 @@
-from django.http.response import HttpResponseRedirect
+from django.contrib.auth import models
+from django.http.response import HttpResponseRedirect, JsonResponse
+from django.views.generic import View
 from django.shortcuts import render
 from django.urls import reverse
-from .models import Forum
+from .models import Comment, Forum
+
+
+from django.contrib.humanize.templatetags.humanize import naturaltime
+from django.utils.text import Truncator
 
 from django.views.decorators.csrf import csrf_exempt
 
@@ -15,6 +21,69 @@ def homeView(request):
     context = {'forums': Forum.objects.all()}
     return render(request, 'home.html', context)
 
+class createForum(View):
+    def get(self, request):
+        authorr = request.user
+        titlee = request.GET.get('title', None)
+        bodyy = request.GET.get('body', None)
+        # time_created = models.DateTimeField(auto_now_add=True),
+        obj = Forum.objects.create(
+            author = authorr,
+            title = titlee,
+            body = bodyy,
+            # time_created = time_created,
+            # time_modified = time_created,
+        )
+        
+        forum = {
+            'pk': obj.pk,
+            'author': obj.author.username,
+            'title': Truncator(obj.title).words(20),
+            'body': obj.body,
+            'time_modified': naturaltime(obj.time_modified),
+        }
+        data = {
+            'forum': forum,
+        }
+        return JsonResponse(data)
+
+class createComment(View):
+    def get(self, request):
+        authorr = request.user
+        titlee = request.GET.get('title', None)
+        bodyy = request.GET.get('body', None)
+        # time_created = models.DateTimeField(auto_now_add=True),
+        obj = Forum.objects.create(
+            author = authorr,
+            title = titlee,
+            body = bodyy,
+            # time_created = time_created,
+            # time_modified = time_created,
+        )
+        
+        forum = {
+            'pk': obj.pk,
+            'author': obj.author.username,
+            'title': obj.titlee,
+            'body': obj.body,
+            'time_modified': naturaltime(obj.time_modified),
+        }
+        data = {
+            'forum': forum,
+        }
+        return JsonResponse(data)
+
+class deleteForum(View):
+    def  get(self, request):
+        pk = request.GET.get('pk', None)
+        Forum.objects.get(pk=pk).delete()
+        data = {
+            'deleted': True
+        }
+        return JsonResponse(data)
+
 def detailView(request, pk):
-    context = {'forum': Forum.objects.get(pk=pk)}
+    context = {
+        'forum': Forum.objects.get(pk=pk),
+        }
     return render(request, 'forum_detail.html', context)
