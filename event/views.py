798b49df2047dce8847f70ceb7400e04983182bf
Christopher Bagas Laiputra <christopher.bagas@ui.ac.id>
2021-12-30 21:20:15 +0700

    update event view

--- event/views.py
+++ event/views.py
@@ -3,6 +3,8 @@ from django.http import JsonResponse, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.core import serializers
+import base64
+from django.core.files.base import ContentFile
 from .models import Event
 from .forms import EventForm
 
@@ -36,3 +38,26 @@ def json(request):
     posts = Event.objects.all()
     response = serializers.serialize('json', posts)
     return HttpResponse(response, content_type = 'application/json')
+
+def add(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        title = data['title']
+        tanggal = data['tanggal']
+        waktu = data['waktu']
+        media = data['media']
+        tipe = data['tipe']
+        Url = data['url']
+        deskripsi = data['deskripsi']
+        file1 = data['file1']
+        name1 = data['name1']
+        image1 = ContentFile(base64.b64decode(file1),name1)
+        file2 = data['file2']
+        name2 = data['name2']
+        image2 = ContentFile(base64.b64decode(file2),name2)
+        try:
+            eventform = Event(Nama=title, Tanggal=tanggal, Waktu = waktu, Media = media, Tipe = tipe, url = Url, Deskripsi = deskripsi, Card_Image = image1, Page_Image = image2)
+            eventform.save()
+            return JsonResponse({"status": "success"}, status=200)
+        except:
+            return JsonResponse({"status": "error"}, status=401)
