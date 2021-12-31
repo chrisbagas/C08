46e980011cc6b98bc76303115206ea161b66c117
unknown <ilhan.firka@ui.ac.id>
2021-12-31 13:09:23 +0700

    update views, models, urls

--- home/views.py
+++ home/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import redirect, render
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, JsonResponse
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from django.core import serializers
 
 from .forms import FeedbackForm
+from .models import FeedbackModel
+
+
 def index(request):
     return render(request, 'index.html')
+
 
 @login_required(login_url='login')
 def feedback(request):
@@ -21,3 +27,23 @@ def feedback(request):
     }
 
     return render(request, 'feedback.html', context)
+
+
+@csrf_exempt
+def json(request):
+    posts = FeedbackModel.objects.all()
+    response = serializers.serialize('json', posts)
+    return HttpResponse(response, content_type='application/json')
+
+
+@csrf_exempt
+def add(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        message = data['message']
+        rating = data['rating']
+        feedbackF = FeedbackModel(message=message, rating=rating)
+        feedbackF.save()
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
